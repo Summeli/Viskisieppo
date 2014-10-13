ce6f9e4011b9696635bf7f0b36573e5345033e82
Summeli <summeli@summeli.fi>
2014-10-13 17:30:12 +0300

    initial commit

---
+++ background.js
@@ -0,0 +1,22 @@
+var requestFilter = {
+	urls: [
+		"*://*/*"
+	]
+};
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	var headers = details.requestHeaders;
+	var header_set = false;
+	for(var i = 0, l = headers.length; i < l; ++i) {
+		if( headers[i].name == "X-Forwarded-for" ) {
+			headers[i].value = "12.13.14.15";
+			header_set = true;
+			break;
+		}
+	}
+	if(!header_set){
+		console.log("uusi!");
+		headers.push({name:"X-Forwarded-for",value:"12.13.14.15"});
+	}
+	return {requestHeaders: headers};
+}, requestFilter, ['requestHeaders','blocking']);
